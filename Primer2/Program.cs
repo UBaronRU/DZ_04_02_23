49716d709394536c0c4e39309445395ca4afaf55
UBaronRU <ubaron@mail.ru>
2023-02-06 13:25:04 +0300

    Primer2 is down

---
+++ Primer2/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+## Primer2
+Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
+*/
+int rows = new Random().Next(5,10);    // количество строк
+int cols = new Random().Next(5,10);    // количество столбцов
+int[,] numbers = new int [rows,cols];
+int tempdig = 0;
+
+fillmas(numbers);
+Console.WriteLine("Начальный массив");
+printmas(numbers);
+for (int i = 0; i < rows; i++)
+{
+    BubbleSort(numbers,i);
+}
+
+
+Console.WriteLine("Обработанный массив");
+printmas(numbers);
+
+void printmas(int[,] arr)
+{
+int rows = arr.GetUpperBound(0) + 1;    // количество строк
+int columns = arr.Length / rows;        // количество столбцов
+ 
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{arr[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+}
+void fillmas(int[,] arr)
+{
+int rows = arr.GetUpperBound(0) + 1;    // количество строк
+int columns = arr.Length / rows;        // количество столбцов
+ 
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        arr[i, j] = new Random().Next(-100,+100);
+    }
+   }
+}
+ void BubbleSort(int[,] array,int rownum)
+{
+int rows = array.GetUpperBound(0) + 1;    // количество строк
+int columns = array.Length / rows;        // количество столбцов	
+    
+    for (int i = 0; i < columns; i++)
+		for (int j = 0; j < columns - 1; j++)
+			if (array[rownum,j] > array[rownum,j + 1])
+			{
+				int t = array[rownum,j + 1];
+				array[rownum,j + 1] = array[rownum,j];
+				array[rownum,j] = t;
+			}
+}
